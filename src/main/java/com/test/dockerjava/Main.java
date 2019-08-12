7848b35a010be25ddafbc3af20999cd4cd36a67a
Nicholas Perkins <nsperkins@spscommerce.com>
2019-08-12 10:45:40 -0500

    simplified

--- src/main/java/com/test/dockerjava/Main.java
+++ src/main/java/com/test/dockerjava/Main.java
@@ -6,13 +6,9 @@ import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
 
 public class Main {
-
-    protected static final String DOCKER_SOCK_PATH = "/var/run/docker.sock";
-    private static final String SOCKET_LOCATION = "unix://" + DOCKER_SOCK_PATH;
-
     public static void main(String[] args) {
         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                .withDockerHost(SOCKET_LOCATION)
+                .withDockerHost("unix:///var/run/docker.sock")
                 .withDockerTlsVerify(false)
                 .withRegistryUrl("https://index.docker.io/v1/")
                 .build();
